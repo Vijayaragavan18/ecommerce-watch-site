c0aa601b42513b9a52276fdc075e5e3c12a3bf43
Vijayaragavan18 <vr81666@gmail.com>
2025-01-06 15:58:45 +0530

    Initial commit

--- src/connect.jsx
+++ src/connect.jsx
@@ -7,7 +7,7 @@ import Collection from "./components/collection/collection";
 import About from "../src/components/aboutPage/about";
 import Footer from "./components/landingPage/footer";
 import ViewPage from './components/landingPage/ViewCartPage.jsx';
-
+import LoginPage from './components/landingPage/logPage.jsx';
 import Cart from './components/cart/Cart';
 import App from './App';
 import Data from './assets/product.json';
@@ -66,7 +66,9 @@ function Connect() {
     const [input, setinput] = useState(dataValue);
 
 
-
+    // setTimeout(() => (
+    //     setvisible(true)
+    // ), 7000)
     return (
 
         <cartContext.Provider value={{ cart, setCart, products, brandImage, visible, dataValue, setvisible, input, setinput, showReg, setShowReg }}>
